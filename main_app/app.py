2e8f0c4ca22c3d6b3733aeee09c0df7d44a9e63c
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2023-08-17 21:36:27 +0900

    Update app.py

--- main_app/app.py
+++ main_app/app.py
@@ -1,26 +1,7 @@
-# Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36
-# import requests
-# from bs4 import BeautifulSoup
-
-# URL = 'https://jp.mercari.com/item/m33273674060'
-# useragent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-# headers = {"User-Agent": useragent}
-
-# resp = requests.get(URL, timeout=1, headers=headers)
-# r_text = resp.text
-
-# soup = BeautifulSoup(r_text, 'html.parser')
-
-
-# print(soup)
-
 import streamlit as st
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome import service as fs
 from selenium.webdriver import ChromeOptions
-from webdriver_manager.core.utils import ChromeType
-from selenium.webdriver.common.by import By
 
 from bs4 import BeautifulSoup
 import time
@@ -35,12 +16,11 @@ if submit:
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    CHROMEDRIVER = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-    service = fs.Service(CHROMEDRIVER)
+    CHROMEDRIVER = ChromeDriverManager().install()  # ChromeTypeを指定しない
     driver = webdriver.Chrome(
-                              options=options,
-                              service=service
-                             )
+        options=options,
+        executable_path=CHROMEDRIVER  # executable_pathを指定する
+    )
 
     # URLで指定したwebページを開く
     driver.get(s_num)
